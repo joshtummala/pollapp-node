b9e28a5b967ffd924640d0e9b85bba4b8486c568
jaswant7 <josh.tummala@gmail.com>
2021-12-15 07:14:07 +0530

    Fixing build

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,55 @@
+import SETTINGS from "./settings";
 
-/**
- * Sample TypeDoc comment for class MyProject
- */
-export class MyProject {
+import express = require("express");
+const app = express();
 
-  /**
-   * sample TypeDoc comment for my method.
-   * Params and Returns will be auto-generated
-   */
-  public static myMethod = (arg1: string, arg2: number): string => {
-    return 'It works!!';
+import bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+import cors = require("cors");
+app.use(cors());
+
+import session = require("express-session");
+app.use(
+  session({
+    secret: SETTINGS.SESSION_SECRET,
+    cookie: {},
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+import mongoose = require("mongoose");
+mongoose.connect(SETTINGS.MONGO_URL);
+
+import userService from "./services/user-service";
+userService(app);
+import groupService from "./services/group-service";
+groupService(app);
+import questionService from "./services/question-service";
+questionService(app);
+
+app.use(
+  (
+    err: { status: any; message: any },
+    req: any,
+    res: {
+      status: (arg0: any) => void;
+      render: (arg0: string, arg1: { message: any; error: {} }) => void;
+    },
+    next: any
+  ) => {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: {},
+    });
   }
+);
 
-}
+app.listen(SETTINGS.PORT, () => {
+  console.log(`Server is running on port ${SETTINGS.PORT}`);
+});
 
-console.log(MyProject.myMethod('', 0));
+export default app;
