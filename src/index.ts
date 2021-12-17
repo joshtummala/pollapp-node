d3b14006b8c007c1b9a8a52fb18c320360549dd8
jaswant7 <josh.tummala@gmail.com>
2021-12-17 11:30:06 +0530

    Fixing cookie issue

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,22 @@ app.use(bodyParser.json());
 
 import cors = require("cors");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 import session = require("express-session");
 import mongodb_session = require("connect-mongodb-session");
